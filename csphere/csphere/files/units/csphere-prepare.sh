8f8710cf3ab3d4c5b440f82898d17974e14311ce
Guangzheng Zhang <zhang.elinks@gmail.com>
2015-10-24 12:11:51 +0800

    finish controller adjust

--- csphere/csphere/files/units/csphere-prepare.sh
+++ csphere/csphere/files/units/csphere-prepare.sh
@@ -6,7 +6,87 @@ FInstOpts="/etc/csphere/inst-opts.env"
 FPublicEnv="/etc/csphere/csphere-public.env"
 
 # load install opts file
-. ${FPublicEnv}
+. ${FInstOpts}
+
+if [ "${COS_ROLE}" == "controller" ]; then
+
+	# create /etc/csphere/csphere-controller.env
+	cat << EOF > /etc/csphere/csphere-controller.env
+ROLE=controller
+AUTH_KEY=${COS_AUTH_KEY}
+DEBUG=true
+DB_URL=mongodb://127.0.0.1:27017
+DB_NAME=csphere
+LISTEN_ADDR=:${COS_CONTROLLER_PORT}
+EOF
+
+	# create /etc/csphere/csphere-agent.env
+	cat << EOF > /etc/csphere/csphere-agent.env
+ROLE=agent
+CONTROLLER_ADDR=127.0.0.1:${COS_CONTROLLER_PORT}
+AUTH_KEY=${COS_AUTH_KEY}
+DEBUG=true
+SVRPOOLID=csphere-internal
+EOF
+
+	# create /etc/prometheus.yml
+	cat << EOF > /etc/prometheus.yml
+global:
+  scrape_interval: 30s
+  evaluation_interval: 45s
+rule_files:
+  - "/data/alarm-rules/*.rule"
+scrape_configs:
+  - job_name: 'csphere-exporter'
+basic_auth:
+  username: 'csphere'
+  password: '${COS_AUTH_KEY}'
+scrape_interval: 30s
+scrape_timeout: 10s
+metrics_path: '/api/metrics'
+target_groups:
+  - targets: ['127.0.0.1:${COS_CONTROLLER_PORT}']
+EOF
+
+	# create /etc/mime.types
+	if [ ! -e /etc/mime.types ]; then
+		ln -sv /usr/lib/csphere/etc/mime.types /etc/mime.types
+	fi
+
+elif [ "${COS_ROLE}" == "agent" ]; then
+	:
+else
+	echo "CRIT: cos role unknown: (${COS_ROLE})"
+fi
+
+# write csphere-public.env
+# br0 IPAddress and Default Route Gateway
+ipaddr=$( ifconfig br0  2>&- |\
+	awk '($1=="inet"){print $2;exit}' )
+if [ -z "${ipaddr}" ]; then
+	echo "WARN: no local ipaddr found on br0"
+fi
+defaultgw=$(route -n 2>&- |\
+	awk '($1=="0.0.0.0" && $4~/UG/){print $2;exit;}' )
+if [ -z "${defaultgw}" ]; then
+	echo "WARN: no local default gateway route found"
+fi
+cat <<EOF > ${FPublicEnv}
+LOCAL_IP=${ipaddr}
+DEFAULT_GW=${defaultgw}
+EOF
+
+
+# promisc br0
+# setup br0 hw ether mac
+ifconfig br0 promisc
+br0inet="$(brctl show br0 2>&- | awk '($1=="br0" && NF==4){print $NF}')"
+br0inetmac="$(ifconfig "${br0inet}" | awk '(/\<ether\>/){print $2}')"
+if [ -n "${br0inetmac}" ]; then
+	ifconfig br0 hw ether "${br0inetmac}"
+else
+	echo "WARN: br0 hw ether mac Null"
+fi
 
 # create ssl key/cert
 fcpem="/data/tls/cert.pem"
@@ -28,60 +108,3 @@ else
 	ln -sv ${fcpem} /root/.csphere/cert.pem
 	/bin/true
 fi
-
-# create /etc/mime.types
-if [ ! -e /etc/mime.types ]; then
-	ln -sv /usr/lib/csphere/etc/mime.types /etc/mime.types
-fi
-
-# write br0 IP and DefaultGw into csphere-public.env
-ipaddr=$( ifconfig br0  2>&- |\
-	awk '($1=="inet"){print $2;exit}' )
-if [ -z "${ipaddr}" ]; then
-	echo "WARN: no local ipaddr found on br0"
-fi
-defaultgw=$(route -n 2>&- |\
-	awk '($1=="0.0.0.0" && $4~/UG/){print $2;exit;}' )
-if [ -z "${defaultgw}" ]; then
-	echo "WARN: no local default gateway route found"
-fi
-cat <<EOF >> ${FPublicEnv}
-LOCAL_IP=${ipaddr}
-DEFAULT_GW=${defaultgw}
-EOF
-
-# promisc br0
-# setup br0 hw ether mac
-ifconfig br0 promisc
-br0inet="$(brctl show br0 2>&- | awk '($1=="br0" && NF==4){print $NF}')"
-br0inetmac="$(ifconfig "${br0inet}" | awk '(/\<ether\>/){print $2}')"
-if [ -n "${br0inetmac}" ]; then
-	ifconfig br0 hw ether "${br0inetmac}"
-else
-	echo "WARN: br0 hw ether mac Null"
-fi
-
-# this file should be create by certain sh
-global:
-  scrape_interval: 30s
-  evaluation_interval: 45s
-rule_files:
-  - "/data/alarm-rules/*.rule"
-scrape_configs:
-  - job_name: 'csphere-exporter'
-basic_auth:
-  username: 'csphere'
-  password: '{CSPHERE_AUTH_KEY}'
-scrape_interval: 30s
-scrape_timeout: 10s
-metrics_path: '/api/metrics'
-target_groups:
-  - targets: ['127.0.0.1:{CSPHERE_CONTROLLER_PORT}']
-
-
-- path: /etc/csphere/csphere-etcd2-early.sh
-  permissions: 0744
-  owner: root
-  content: |
-    #!/bin/sh
-    set -ex
